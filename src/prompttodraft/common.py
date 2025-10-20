603cb0076c6455bc9099670cd54889f7b3c8356a
Charles Azam <azamcharles0@gmail.com>
2025-10-20 18:54:26 +0200

    feat: main.py

---
+++ src/prompttodraft/common.py
@@ -0,0 +1,6 @@
+from datetime import date
+from pathlib import Path
+
+PROMPT_TO_DRAFT_PATH = Path(__file__).parent
+PROMPT_TO_DRAFT_REPO_PATH = PROMPT_TO_DRAFT_PATH.parent.parent
+assert (PROMPT_TO_DRAFT_REPO_PATH/"playground").exists(), "Playground directory does not exist"
