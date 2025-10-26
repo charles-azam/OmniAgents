4d6ffa043c3eaa4233701a89f3e4cf9d11d9ef51
Charles AZAM <azamcharles0@gmail.com>
2025-10-26 14:20:01 +0100

    Pipeline (#6)
    * feat: add pipeline
    
    * fix: data path
    
    * cancel in progress

--- tests/test_storage_utils.py
+++ tests/test_storage_utils.py
@@ -1,7 +1,8 @@
+from prompttodraft.common import DATA_PATH
 from prompttodraft.storage_utils import write_to_storage, read_from_storage
 from pathlib import Path
 
 def test_storage_utils():
-    file_path = Path("test.txt")
+    file_path = DATA_PATH / "test.txt"
     write_to_storage(file_path, "test")
     assert read_from_storage(file_path) == "test"
